d5c9f949b1c3f3e5a6ced44f9efbd5b6edb2b3a1
Anne van den Berg <avdberg@anwb.nl>
2018-05-30 20:32:58 +0200

    Removed unnecassary files and improved CounterDemo

--- CounterDemo.elm
+++ CounterDemo.elm
@@ -1,11 +1,19 @@
 module Main exposing (..)
 
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+import Css exposing (..)
+import Css.Foreign
+import Html exposing (beginnerProgram)
+import Html.Styled exposing (Attribute, Html, br, button, div, h2, img, styled, text, toUnstyled)
+import Html.Styled.Attributes exposing (..)
+import Html.Styled.Events exposing (onClick)
 
 
 main =
-    Html.beginnerProgram { model = model, view = view, update = update }
+    Html.beginnerProgram
+        { model = model
+        , view = view >> toUnstyled
+        , update = update
+        }
 
 
 
@@ -23,9 +31,6 @@ model =
 
 
 -- UPDATE
-{-
-   TODO 1: Add a new Reset Msg option
--}
 
 
 type Msg
@@ -35,14 +40,6 @@ type Msg
     | Reset
 
 
-
-{-
-   TODO 2: Add a new Reset case
-   TODO 4: Prevent Decrement from going below 0
-   TODO 5: Build new cases or add new functionality for a counter (e.g. +2 or -10)
--}
-
-
 update : Msg -> Model -> Model
 update msg model =
     case msg of
@@ -50,7 +47,7 @@ update msg model =
             model + 1
 
         Decrement ->
-            max (model - 1) 0
+            Basics.max (model - 1) 0
 
         Square ->
             model * model
@@ -66,18 +63,79 @@ increment a =
 
 
 -- VIEW
-{-
-   TODO 3: Add a new button which will trigger the reset action on click
--}
 
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ button [ onClick Decrement ] [ text "-" ]
-        , div [] [ text (toString model) ]
+    page []
+        [ h2 [] [ text "Count of yawns in the room" ]
+        , img [ src "https://media.giphy.com/media/j6Uls25PT5Q4g/giphy.gif" ] []
+        , br [] []
+        , div [] [ text ("Count: " ++ toString model) ]
+        , button [ onClick Decrement ] [ text "-" ]
         , button [ onClick Increment ] [ text "+" ]
         , button [ onClick Square ] [ text "*" ]
-        , Html.br [] []
+        , br [] []
         , button [ onClick Reset ] [ text "reset" ]
+        ]
+
+
+
+-- CSS
+
+
+page : List (Attribute msg) -> List (Html msg) -> Html msg
+page attributes children =
+    styled div
+        [ padding (pct 2) ]
+        attributes
+        (style1 :: children)
+
+
+style1 =
+    Css.Foreign.global
+        [ Css.Foreign.body
+            [ Css.width (px 500)
+            , margin2 (px 0) auto
+            , fontFamilies [ "Arial", "Sans Sherif", "Helvetica" ]
+            , fontSize (px 13)
+            , color (hex "1C1C1C")
+            , textAlign center
+            ]
+        , Css.Foreign.header
+            [ marginBottom (Css.rem 1)
+            ]
+        , Css.Foreign.h2
+            [ textAlign center
+            , color (hex "000000")
+            ]
+        , Css.Foreign.input
+            [ display inline
+            , marginBottom (Css.rem 1)
+            , padding (Css.rem 0.2)
+            , Css.height (Css.rem 2)
+            , border3 (Css.px 1) solid (hex "1C1C1C")
+            , borderRadius (Css.px 5)
+            , Css.width (pct 50)
+            ]
+        , Css.Foreign.button
+            [ margin (Css.rem 1)
+            , display inlineBlock
+            , Css.height (Css.rem 2)
+            , border3 (px 1) solid (hex "1C1C1C")
+            , borderRadius (px 5)
+            ]
+        , Css.Foreign.class "buttonAdd"
+            [ backgroundColor (hex "c8e782")
+            ]
+        , Css.Foreign.ul
+            [ marginTop (Css.rem 1)
+            , padding (px 0)
+            ]
+        , Css.Foreign.li
+            [ marginBottom (Css.rem 1)
+            , padding (Css.rem 1)
+            , listStyleType none
+            , border3 (px 1) solid (hex "000000")
+            ]
         ]
